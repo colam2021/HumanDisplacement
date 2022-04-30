ee6e05bf249f9e4d5ba79d1e977a154c04a0f084
colam2021 <onlam.study@gmail.com>
2022-04-30 16:50:57 -0700

    commit

---
+++ HD.R
@@ -0,0 +1,45 @@
+lib = c("tidyverse","networkD3", "shiny")
+lapply(lib, require, character.only = TRUE) 
+
+links = read.csv("HD.csv", header = T)
+
+nodes = data.frame(name = c(as.character(links$source),
+                            as.character(links$target))
+                   %>% unique(),
+                   stringsAsFactors = FALSE)
+
+
+links$IDsource <- match(links$source, nodes$name)-1
+
+links$IDtarget <- match(links$target, nodes$name)-1
+
+
+p <- sankeyNetwork(Links = as.data.frame(links), Nodes = as.data.frame(nodes),
+                   
+                   Source = "IDsource", Target = "IDtarget",
+                   
+                   Value = "value", NodeID = "name", height=800, width=1000,
+                   
+                   sinksRight=FALSE,  fontSize=14, fontFamily="Arial", nodeWidth = 50, nodePadding=10, iterations = 0)
+
+p
+
+ui = shinyUI(fluidPage(
+  
+  h3("Human Displacement 2020", style="text-align:left"),
+  h4("In 2020, there were 18.9 million refugees under UNHCR's mandate.  Three quarters of these refugees were come from Syrian, Afghanistan, South Sudan, Myanmar, Somalia, and DR of Congo due to wars.  Most often, refugees were hosted by neighbouring or nearby countriies.    "),
+  
+  h5("Source: UNHCR Refugee Data Finder https://www.unhcr.org/refugee-statistics/download/?url=5I6XeJ", style="text-align:left"),
+  
+  sankeyNetworkOutput('sankey')
+  
+))
+
+
+server = function(input, output) {
+  
+  output$sankey <- renderSankeyNetwork(p)
+  
+}
+
+shinyApp(ui = ui, server = server)
